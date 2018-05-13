6c5133af47d02e44451c6076889dda7a4f08a85f
theelectricfire7 <androidisawesome.apk@gmail.com>
2018-05-13 18:58:53 +1000

    Init server and client

---
+++ server/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "no-unused-vars": 0,
+        "indent": 0,
+        "no-console": 1,
+        "no-undef": 1,
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
